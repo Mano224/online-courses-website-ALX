a6570b6b5e7bd3020aee13d828e2166bedb25209
Mano224 <mano@example.com>
2025-01-05 17:51:16 +0200

    update for contact

--- client-side/src/Contact/Contact.jsx
+++ client-side/src/Contact/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
         return;
       }
 
-      const response = await fetch('/contact', {
+      const response = await fetch('http://localhost:5000/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
